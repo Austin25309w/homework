413c8fbdd271fa258eb54a51d3040b6e0ef64cbc
Austin25309w <austin@austins-MacBook-Pro.local>
2016-04-18 23:02:38 -0700

    carousel_FINAL

---
+++ carousel/js/main.js
@@ -0,0 +1,77 @@
+// Create an array of image paths
+
+
+var images = ["images/image_1.jpg", "images/image_2.jpg","images/image_3.jpg","images/image_4.jpg","images/image_5.jpg","images/image_6.jpg"];
+// set a variable for our currentPosition
+
+var currentPosition = 0;
+
+// go to the next image when the user click next.
+
+$('#next').on('click', function() {
+//add one to the currentPosition
+
+	currentPosition += 1;
+	// if currentPosition is the last index
+			// Disable the next button
+
+	if (currentPosition ===  images.length - 1) {
+		$('#next').prop('disabled', true);
+	}
+	else {
+		$('#next').prop('disabled', false);
+		$('#prev').prop('disabled', false);
+	}
+	//change the src of #image-to-vote-on to current image
+	$('#image-to-vote-on').attr('src', images[currentPosition]); 
+
+});
+	
+// go to the pervious image when the user click next.
+$('#prev').on('click', function() {
+	currentPosition -= 1;
+
+	if (currentPosition ===  0) {
+		$('#prev').prop('disabled', true);
+	}
+	else {
+		$('#prev').prop('disabled', false);
+		$('#next').prop('disabled', false);
+	}
+	$('#image-to-vote-on').attr('src', images[currentPosition]); 
+});
+//add to the like of the current image when user clicks the thumbs up.
+
+// */var 
+var counter = 0;
+$("#upvote").click(function() {
+	counter ++;
+	$('#votes').html('Likes: ' + counter);
+});
+
+
+$("#downvote").click(function() {
+	counter --;
+	$("#votes").html('Likes: ' + counter);
+
+});
+
+
+
+// 	votes += 1;
+// 	if(votes === 0) {
+// 		$('#upvote')
+// 		}
+// 	else
+
+// 	}
+// 	currentPosition += 1;
+// 	$('#votes').
+// });
+
+// $('#downvote').on('click', function () {
+
+// })
+//remove from the like of the current image when user clicks the thumbs down.
+
+
--- temp_converter/js/main.js
+++ temp_converter/js/main.js
@@ -44,30 +44,30 @@ $('#fahrenheit_to_celsius').on('click', function () {
 //Get the value from #temperature and store in variable celsius
 $('#celsius_to_fahrenheit').on('click', function () {
 	console.log("answer")
-//convert that value to a number
-var celsius = $('#temperature').val();
-//get the formula
-var answer = 1.8 * celsius + 32;
-//convert fahrenheit and store in variable fahrenheit
-console.log(answer)
-// Display in result
-$('#result').html(celsius + '&#176C = ' + answer + '&#176F');
+	//convert that value to a number
+	var celsius = $('#temperature').val();
+	//get the formula
+	var answer = 1.8 * celsius + 32;
+	//convert fahrenheit and store in variable fahrenheit
+	console.log(answer)
+	// Display in result
+	$('#result').html(celsius + '&#176C = ' + answer + '&#176F');
 
 
 
 
-if (celsius === '')  {
+	if (celsius === '')  {
 
-alert('enter a temperature');
-return false
-}
+	alert('enter a temperature');
+	return false
+	}
 
-if (celsius < 5) 	{
-	$('body').addClass('blue');
-}
+	if (celsius < 5) 	{
+		$('body').addClass('blue');
+	}
 
-if (celsius > 6) {
-	$('body').addClass('red');
-}
+	if (celsius > 6) {
+		$('body').addClass('red');
+	}
 });
 
